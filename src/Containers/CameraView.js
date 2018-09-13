a2b778f6f9d494b4d553f8baaeced955fd8eb41c
Adam <ahkolodko@myseneca.ca>
2018-09-13 18:44:36 -0400

    Add button

--- src/Containers/CameraView.js
+++ src/Containers/CameraView.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { Text, View, TouchableOpacity } from 'react-native';
-import { Camera, Permissions } from 'expo';
+import { Camera, FileSystem, Permissions } from 'expo';
 
 export default class CameraView extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
+    photoName: "No photo taken",
   };
 
   async componentWillMount() {
     const { status } = await Permissions.askAsync(Permissions.CAMERA);
     this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  componentDidMount() {
+    FileSystem.makeDirectoryAsync(FileSystem.documentDirectory + 'photos').catch(e => {
+      console.log(e, 'Directory exists');
+    });
+  }
+
+  takePicture = () => {
+    if (this.camera) {
+      this.camera.takePictureAsync({ onPictureSaved: this.onPictureSaved });
+    }
+  };
+
+  onPictureSaved = async photo => {
+    await FileSystem.moveAsync({
+      from: photo.uri,
+      to: `${FileSystem.documentDirectory}photos/${Date.now()}.jpg`,
+    });
+    this.setState({ photoName: "Photo clicked" });
   }
 
   render() {
@@ -22,32 +43,30 @@ export default class CameraView extends React.Component {
     } else {
       return (
         <View style={{ flex: 1 }}>
-          <Camera style={{ flex: 1 }} type={this.state.type}>
+          <Camera 
+            style={{ flex: 1 }} 
+            type={this.state.type}
+            ref={ref => {
+              this.camera = ref;
+            }}>
             <View
               style={{
                 flex: 1,
                 backgroundColor: 'transparent',
-                flexDirection: 'row',
+                flexDirection: 'row'
               }}>
               <TouchableOpacity
                 style={{
-                  flex: 0.1,
+                  flex: 1,
                   alignSelf: 'flex-end',
-                  alignItems: 'center',
+                  alignItems: 'center'
                 }}
-                onPress={() => {
-                  this.setState({
-                    type: this.state.type === Camera.Constants.Type.back
-                      ? Camera.Constants.Type.front
-                      : Camera.Constants.Type.back,
-                  });
-                }}>
+                onPress={this.takePicture}>
                 <Text
-                  style={{ fontSize: 18, marginBottom: 10, color: 'white',flex: 1,
-                  alignSelf: 'flex-end',
-                  alignItems: 'center', }}>
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white', backgroundColor: 'steelblue'
+                }}>
                   
-                  {' '}Bottom Text Example{' '}
+                  { this.state.photoName }
 
                 </Text>
               </TouchableOpacity>
